bc74b37b347dc3cc73c49cbba595ee713449e8d2
Dennis Schneider <d.schneider.ulm@gmx.de>
2022-11-14 18:40:25 +0100

    FEAT: Heads working

--- kadapter/configurations.py
+++ kadapter/configurations.py
@@ -3,15 +3,15 @@ import copy
 
 
 class AdapterConfig(BertConfig):
-    
+
     def __init__(self,
                  model_name='adapter',
                  hidden_dimension=120,
-                 injection_layers = (0,11),
-                 skip_layers = 3,
-                 initializer_range = 0.0002,
-                 num_hidden_layers = 2,
-                 freeze = False,
+                 injection_layers=(0, 11),
+                 skip_layers=3,
+                 initializer_range=0.0002,
+                 num_hidden_layers=2,
+                 freeze=False,
                  **kwargs):
         super().__init__(**kwargs)
         self.model_name = model_name
@@ -23,20 +23,48 @@ class AdapterConfig(BertConfig):
         self.freeze = freeze
 
 
+"""
 class KAdapterHeadConfig(PretrainedConfig):
     model_type = 'kadapter-head-sum'
-    
-    def __init__(self, 
+
+    def __init__(self,
                  head_type: str = 'kadapter-head-sum',
                  **kwargs):
         self.head_type = head_type
+        super().__init__(**kwargs)
+"""
+
+
+class KAdapterHeadConfig:
+
+    def __init__(self,
+                 p_dropout=0.5,
+                 **kwargs,
+                 ):
+        super().__init__(**kwargs)
+        self.p_dropout = p_dropout
+
+
+class KAdapterSumHeadConfig(KAdapterHeadConfig, PretrainedConfig):
+    model_type = 'kadapter-head-sum'
+
+    def __init__(self,
+                 **kwargs):
+        super().__init__(**kwargs)
+
+
+class KAdapterConcatHeadConfig(KAdapterHeadConfig, PretrainedConfig):
+    model_type = 'kadapter-head-concat'
+
+    def __init__(self,
+                 **kwargs):
         super().__init__(**kwargs)
 
 
 class KAdapterConfig(PretrainedConfig):
     model_type = 'kadapter'
-    is_composition=True
-    
+    is_composition = True
+
     def __init__(self, **kwargs):
         self.freeze_basemodel = False
         basemodel_config = kwargs.pop('basemodel')
@@ -45,14 +73,14 @@ class KAdapterConfig(PretrainedConfig):
         head_config = kwargs.pop('head')
         head_config['n_adapters'] = len(self.adapters)
         head_config['hidden_size'] = self.basemodel.hidden_size
-        self.head = KAdapterHeadConfig(**head_config)
+        self.head = AutoConfig.for_model(head_config.pop('model_type'), **head_config)
         super().__init__(**kwargs)
 
     @classmethod
-    def from_adapter_configs(cls, basemodel_config: PretrainedConfig, adapter_configs : list, head_config : KAdapterHeadConfig):
-        adapter_config_dicts = [config.to_dict() for config in adapter_configs]
-        return cls(basemodel=basemodel_config.to_dict(), adapters=adapter_config_dicts, head=head_config.to_dict())
-    
+    def from_adapter_configs(cls, basemodel: PretrainedConfig, adapters: list, head: PretrainedConfig):
+        adapter_config_dicts = [config.to_dict() for config in adapters]
+        return cls(basemodel=basemodel.to_dict(), adapters=adapter_config_dicts, head=head.to_dict())
+
     def to_dict(self):
         output = copy.deepcopy(self.__dict__)
         output['basemodel'] = self.basemodel.to_dict()
@@ -60,4 +88,3 @@ class KAdapterConfig(PretrainedConfig):
         output['head'] = self.head.to_dict()
         output['model_type'] = self.__class__.model_type
         return output
-    
--- kadapter/head.py
+++ kadapter/head.py
@@ -1,16 +1,16 @@
-from .configurations import KAdapterHeadConfig
+from .configurations import KAdapterSumHeadConfig, KAdapterConcatHeadConfig, KAdapterHeadConfig
 
-from transformers import PreTrainedModel, AutoConfig, AutoModel
+from transformers import PreTrainedModel, PretrainedConfig, AutoConfig, AutoModel
 import torch
 
 
 class KAdapterHead:
-    
-    def __init__(self, 
+
+    def __init__(self,
                  config: KAdapterHeadConfig):
         super().__init__(config)
-        self.dropout = torch.nn.Dropout()
-        
+        self.dropout = torch.nn.Dropout(config.p_dropout)
+
     def forward(self, adapter_outputs: torch.Tensor):
         output = self.combine(torch.stack(adapter_outputs))
         assert output.shape == adapter_outputs.shape
@@ -18,10 +18,10 @@ class KAdapterHead:
         # TODO this transforms from the entire output to num_labels!
         # output = self.linear(output[:, 0, :].squeeze(1))
         return output
-    
+
     def combine(self, adapter_outputs):
         pass
-    
+
     def get_output_embeddings(self):
         raise NotImplementedError()
 
@@ -30,8 +30,8 @@ class SumHead(KAdapterHead, PreTrainedModel):
     """
     Sums up the outputs of Adapters.
     """
-    config_class = KAdapterHeadConfig
-    
+    config_class = KAdapterSumHeadConfig
+
     def combine(self, adapter_outputs: torch.Tensor):
         return adapter_outputs.sum(0)
 
@@ -40,28 +40,24 @@ class ConcatHead(KAdapterHead, PreTrainedModel):
     """
     Concatenates the outputs of Adapters
     """
-    config_class = KAdapterHeadConfig
-    
-    
-    def __init__(self, 
-                 config: KAdapterHeadConfig):
+    config_class = KAdapterConcatHeadConfig
+
+    def __init__(self,
+                 config: KAdapterConcatHeadConfig):
         super().__init__(config)
-        print(config)
         self.n_adapters = config.n_adapters
         self.hidden_size = config.hidden_size
-        self.linear_out = torch.nn.Linear(2*self.n_adapters*self.hidden_size, self.hidden_size)
-        
+        self.linear_out = torch.nn.Linear(2 * self.n_adapters * self.hidden_size, self.hidden_size)
+
     def combine(self, adapter_outputs: torch.Tensor):
-        hidden = torch.concat([adapter_outputs[1:], adapter_outputs[0].repeat(self.n_adapters,1,1,1)], -1)
+        hidden = torch.concat([adapter_outputs[1:], adapter_outputs[0].repeat(self.n_adapters, 1, 1, 1)], -1)
         assert adapter_outputs.dim() == 4
         output = torch.cat(torch.unbind(hidden), -1)
         return self.linear_out(output)
 
-    
-from .configurations import KAdapterHeadConfig
 
-AutoConfig.register("kadapter-head-sum", KAdapterHeadConfig)
-AutoModel.register(KAdapterHeadConfig, SumHead)
+AutoConfig.register("kadapter-head-sum", KAdapterSumHeadConfig)
+AutoModel.register(KAdapterSumHeadConfig, SumHead)
 
-#AutoConfig.register("kadapter-head-concat", KAdapterHeadConfig)
-#AutoModel.register(KAdapterHeadConfig, ConcatHead)
+AutoConfig.register("kadapter-head-concat", KAdapterConcatHeadConfig)
+AutoModel.register(KAdapterConcatHeadConfig, ConcatHead)
